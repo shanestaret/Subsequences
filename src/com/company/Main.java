8d249c04467cb98a84c7c564c4def1d09196cf66
Shane Staret <shanestaret@gmail.com>
2019-01-27 05:52:32 -0500

    Added detailed comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,7 @@ public class Main {
 		String s1 = "";
 		String s2 = "";
 
+		//continue asking user for more strings to compare unless they enter "-1" to exit
 		while(true) {
 			System.out.print("Enter in first string (enter \"-1\" to exit): ");
 			s1 = input.next();
@@ -25,17 +26,21 @@ public class Main {
 				System.exit(0);
 			}
 
+			//arrays that will hold subsequences of each string
 			ArrayList<String> s1Array = new ArrayList();
 			ArrayList<String> s2Array = new ArrayList();
 
+			//methods that get all subsequences of each string, give the String and the ArrayList that will hold all subsequences of the String
 			Subs.subsequence(s1, s1Array);
 			Subs.subsequence(s2, s2Array);
 
 			System.out.println("\n----------------------------------------");
 
+			//printing all subsequences of each string
 			System.out.println("All subsequences of first string: " + s1Array.toString());
 			System.out.println("All subsequences of second string: " + s2Array.toString());
 
+			//printing longest common subsequences between both Strings, must give method both ArrayLists that previously had all of their subsequences determined
 			System.out.println("List of the longest common subsequences between each string: " + Subs.longestSubsequence(s1Array, s2Array) + "\n----------------------------------------\n");
 		}
 	}
--- src/com/company/Subs.java
+++ src/com/company/Subs.java
@@ -4,19 +4,21 @@ import java.util.ArrayList;
 
 public class Subs {
 
+    //method that determines all subsequences given a string "s", puts all subsequences into ArrayList "sArray"
     public static void subsequence(String s, ArrayList<String> sArray) {
 
         //begin the substring at the first letter
         for (int i = 0; i < s.length(); i++) {
-            //end the string starting with the last letter, then work way in
+            //end the substring starting with the last letter, then working way in
             for (int j = s.length(); j > i; j--) {
                 String sub = s.substring(i, j);
 
+                //if this substring is new, put it into the ArrayList
                 if (!sArray.contains(sub)) {
                     sArray.add(sub);
                 }
 
-                // delete one character of the given substring to create another substring and repeat the entire process for that substring...recursion
+                //delete one character of the given substring to create another substring and repeat the entire process for that substring...recursion
                 for (int k = 1; k < sub.length() - 1; k++) {
                     String subPart = sub.substring(0, k) + sub.substring(k + 1, sub.length());
                     subsequence(subPart, sArray);
@@ -25,18 +27,25 @@ public class Subs {
         }
     }
 
+    //method that returns the longest common subsequences between both Strings, must be given both ArrayLists that have contain all subsequences of each String
     public static ArrayList<String> longestSubsequence(ArrayList<String> s1Array, ArrayList<String> s2Array) {
+        //by default, the longest subsequence must be at least 1 character
         int longestInt = 1;
+        //creation of new ArrayList that will hold the longest common subsequences between both Strings
         ArrayList<String> longestStrings = new ArrayList();
 
+        //nested loops that compare values in each given ArrayList and add a value to "longestStrings" if it is common to both given ArrayLists, clear "longestStrings" if a longer common subsequence is found and then add that new subsequence to "longestStrings"
         for(int i = 0; i < s1Array.size(); i++) {
             for(int j = 0; j < s2Array.size(); j++) {
+                //if a value is found that is common to both ArrayLists AND this value has not already been found, then add it to "longestStrings"
                 if(s1Array.get(i).equals(s2Array.get(j)) && !longestStrings.contains(s1Array.get(i))) {
+                    //if this new value is longer than any other value currently in "longestStrings", clear "longestStrings" and add this new value to it
                     if(s1Array.get(i).length() > longestInt) {
                         longestStrings.clear();
                         longestStrings.add(s1Array.get(i));
                         longestInt = s1Array.get(i).length();
                     }
+                    //otherwise, if this new value is of equal length to the values currently in "longestStrings", add this new value to it
                     else if(s1Array.get(i).length() == longestInt)
                         longestStrings.add(s1Array.get(i));
                 }
